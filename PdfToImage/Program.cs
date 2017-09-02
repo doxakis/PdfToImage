60bf41232a2a55383a2365451c40d0f59733e80f
Philip Doxakis <doxakis@users.noreply.github.com>
2017-09-02 12:46:33 -0400

    feat: convert pdf to jpg

--- PdfToImage/Program.cs
+++ PdfToImage/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using PdfiumViewer;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,38 @@ namespace PdfToImage
 	{
 		static void Main(string[] args)
 		{
+			var projectDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+			try
+			{
+				using (var document = PdfDocument.Load(projectDir + @"\Samples\input.pdf"))
+				{
+					var pageCount = document.PageCount;
+
+					for (int i = 0; i < pageCount; i++)
+					{
+						var dpi = 300;
+						
+						using (var image = document.Render(i, dpi, dpi, PdfRenderFlags.CorrectFromDpi))
+						{
+							var encoder = ImageCodecInfo.GetImageEncoders()
+								.First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+							var encParams = new EncoderParameters(1);
+							encParams.Param[0] = new EncoderParameter(
+								System.Drawing.Imaging.Encoder.Quality, 100L);
+
+							image.Save(projectDir + @"\Samples\output_" + i + ".jpg", encoder, encParams);
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			
+			Console.WriteLine("Press enter to continue...");
+			Console.ReadLine();
 		}
 	}
 }
